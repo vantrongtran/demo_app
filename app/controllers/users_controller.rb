a82c158bae0ed5257e77a94114af69d2f084d39f
vantrongtran <vntrngtrn@yahoo.com.vn>
2016-09-27 22:21:35 +0700

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,10 @@ class UsersController < ApplicationController
   def create
 	@user = User.new(user_params)
 	if @user.save
-
+    flash[:success] = "Welcome to Blog"
+    redirect_to @user
 	else
-	  render new	
+	  render 'new'	
 	end
   end
 
