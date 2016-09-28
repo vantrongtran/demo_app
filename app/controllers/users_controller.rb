640fbbc412958adfc39f788624f68f470e8a3a6b
vantrongtran <vntrngtrn@yahoo.com.vn>
2016-09-28 11:24:57 +0700

    user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,54 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
 
   def new
-	@user = User.new
+	  @user = User.new
   end
 
   def create
-	@user = User.new(user_params)
-	if @user.save
-    flash[:success] = "Welcome to Blog"
-    redirect_to @user
-	else
-	  render 'new'	
-	end
+	  @user = User.new(user_params)
+	  if @user.save
+      flash[:success] = "Welcome to Blog"
+      redirect_to @user
+	  else
+	    render 'new'	
+	  end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Update success!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def index
-	@user = User.all
+	@users = User.paginate(page: params[:page],:per_page => 10)
   end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please login"
+      redirect_to login_path
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user?(user)
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
