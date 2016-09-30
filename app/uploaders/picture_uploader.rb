25314379cfd16499fc67b72b3d75f6a3d6cf4097
vantrongtran <vntrngtrn@yahoo.com.vn>
2016-09-30 11:06:41 +0700

    fix upload file

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,11 +9,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  storage :file
+  # end
 
   # storage :fog
 
